cefd16b1fdd80a5e33832b3df64739cbae294d21
Gabriel Lucas <dreadpool38@gmail.com>
2023-06-20 15:29:45 -0300

    fix: faz ajustes caroulsel de sports

--- components/SportsCards/Card.tsx
+++ components/SportsCards/Card.tsx
@@ -1,16 +1,21 @@
 import Image from 'next/image';
 import { ReactNode } from 'react';
+import cn from 'classnames';
 
 type Props = {
   children: ReactNode;
   icon: string;
   bgImage: string;
+  cardsQuantity: number;
 };
-export default function Card({ children, icon, bgImage }: Props) {
+export default function Card({ children, icon, bgImage, cardsQuantity }: Props) {
   return (
     <div
-      className=' rounded px-7 py-7 bg-gray-dark flex items-center justify-center
-    gap-[5px] tablet:gap-[7px] w-[110px] h-[48px] relative overflow-hidden  tablet:w-[130px] tablet:h-[56px] desktop:w-[150px] '
+      className={cn(`rounded px-7 py-7 bg-gray-dark flex items-center justify-center
+    gap-[5px] tablet:gap-[7px] w-[110px] h-[48px] relative overflow-hidden  tablet:w-[130px] tablet:h-[56px] tabletg:w-[150px]`,
+        {
+          'tablet:w-[100px]': cardsQuantity >= 4,
+        })}
     >
       <div className='w-full h-full absolute top-0 left-0'>
         <Image fill alt='imagem de fundo' src={bgImage}></Image>
--- components/SportsCards/SportsCardsUi.tsx
+++ components/SportsCards/SportsCardsUi.tsx
@@ -6,9 +6,9 @@ import { getSportCarousel } from '../../lib/api';
 export default async function SportsCards() {
   const data = await getSportCarousel();
   return (
-      <div className=' flex gap-6'>
-        {data.slice(0,3).map(({ name, sportIcon, sportImage }) => (
-          <Card key={name} icon={sportIcon} bgImage={sportImage}>
+      <div className='flex gap-6'>
+        {data.slice(0,4).map(({ name, sportIcon, sportImage }) => (
+          <Card key={name} icon={sportIcon} bgImage={sportImage} cardsQuantity={data.length}>
             {name}
           </Card>
         ))}
