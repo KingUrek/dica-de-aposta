12b083e1fb3c07b4fa436772e26f29234f79f735
Gabriel Lucas <dreadpool38@gmail.com>
2023-06-21 15:40:20 -0300

    fix: termina caroulsel de esportes

--- components/SportsCards/Card.tsx
+++ components/SportsCards/Card.tsx
@@ -6,30 +6,44 @@ type Props = {
   children: ReactNode;
   icon: string;
   bgImage: string;
-  cardsQuantity: number;
+  isSmall: boolean;
 };
-export default function Card({ children, icon, bgImage, cardsQuantity }: Props) {
+export default function Card({
+  children,
+  icon,
+  bgImage,
+  isSmall,
+}: Props) {
   return (
     <div
-      className={cn(`rounded px-7 py-7 bg-gray-dark flex items-center justify-center
-    gap-[5px] tablet:gap-[7px] w-[110px] h-[48px] relative overflow-hidden  tablet:w-[130px] tablet:h-[56px] tabletg:w-[150px]`,
+      className={cn(
+        `rounded  py-7 bg-gray-dark flex items-center justify-center
+    gap-[5px] tablet:gap-[7px] w-[110px] h-[48px] relative overflow-hidden   tablet:h-[56px] tabletg:w-[150px]`,
         {
-          'tablet:w-[100px]': cardsQuantity >= 4,
-        })}
+          'tablet:w-[100px] px-4': isSmall,
+          'tablet:w-[130px] px-7': !isSmall,
+        }
+      )}
     >
       <div className='w-full h-full absolute top-0 left-0'>
         <Image fill alt='imagem de fundo' src={bgImage}></Image>
       </div>
       <div className='w-full h-full absolute top-0 left-0 bg-black-darkest bg-opacity-60 z-10'></div>
-      <div className='relative w-10 h-10 tablet:w-12 tablet:h-12'>
-        <Image
-          className=' z-20'
- fill
-          alt='sport icon'
-          src={icon}
-        />
+      <div className={cn('relative  shrink-0',         {
+          'w-8 h-8 tabletg:w-12 tabletg:h-12': isSmall,
+          'w-10 h-10 tablet:w-12 tablet:h-12': !isSmall,
+        })}>
+        <Image className=' z-20' fill alt='sport icon' src={icon} />
       </div>
-      <p className=' text-16 tablet:text-20 text-white font-bold font-tittilium z-20'>
+      <p
+        className={cn(
+          '   text-white font-bold font-tittilium z-20 ',
+          {
+            'text-12 mobileg:text-16 tabletg:text-20': isSmall,
+            'text-16 tablet:text-20': !isSmall,
+          }
+        )}
+      >
         {children}
       </p>
     </div>
--- components/SportsCards/SportsCardsUi.tsx
+++ components/SportsCards/SportsCardsUi.tsx
@@ -2,13 +2,21 @@ import React from 'react';
 import Container from '../ui/container';
 import Card from './Card';
 import { getSportCarousel } from '../../lib/api';
+import cn from 'classnames';
+
 
 export default async function SportsCards() {
-  const data = await getSportCarousel();
+  let data = await getSportCarousel();
+  data = [...data, ...data]
+  const isSmall = data.length >= 4;
+
   return (
-      <div className='flex gap-6'>
+    <div className={cn('flex ', {
+      'gap-4': isSmall,
+      'gap-6':!isSmall
+      })}>
         {data.slice(0,4).map(({ name, sportIcon, sportImage }) => (
-          <Card key={name} icon={sportIcon} bgImage={sportImage} cardsQuantity={data.length}>
+          <Card key={name} icon={sportIcon} bgImage={sportImage} isSmall={isSmall}>
             {name}
           </Card>
         ))}
